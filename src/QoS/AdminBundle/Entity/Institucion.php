91167f9e24b02a8860d67bf0d487fb3a26aa85df
Hender Orlando <hender.puello@gmail.com>
2014-10-20 18:37:54 -0500

    Arreglado Medicion null
    Arreglado cuando la medición es null

--- src/QoS/AdminBundle/Entity/Institucion.php
+++ src/QoS/AdminBundle/Entity/Institucion.php
@@ -184,7 +184,10 @@ class Institucion extends Objeto
         if($humanize){
             return $medicion->humanize($promedio/$count).'/seg';
         }
-        return $medicion->humanize($promedio/$count,false);//byte/seg
+        if($medicion){
+            return $medicion->humanize($promedio/$count,false);//byte/seg
+        }
+        return 0;
     }
 
     /**
